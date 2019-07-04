db27f4687134ba3afed3f08805bde4340aa7aa27
Vinz <vincentballut@outlook.fr>
2019-07-04 10:42:21 +0200

    continue

--- project-name/src/cats/cats.controller.ts
+++ project-name/src/cats/cats.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get, Post, HttpCode, Header } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, Header, Param } from '@nestjs/common';
 import { get } from 'http';
 import { create } from 'domain';
 
 @Controller('cats')
 export class CatsController {
+
+    // // renvoi simple sans async
+    // @Get()
+    // findAll(): string {
+    //     return 'This action returns all cats';
+    // }
+
+    // // renvoi avec async
     @Get()
-    findAll(): string {
-        return 'This action returns all cats';
+    async findAll(): Promise<any[]> {  // la promesse renvoi un tableau
+        return [];
+    }
+
+    // // permet de d'utiliser plusieurs type de paramètres
+    // @Get(':id')
+    // findOne(@Param() params): string {
+    //     return `This action return a #${params.id} cat`;
+    // }
+
+    @Get(':id')
+    findOne(@Param('id') id): string {
+        return `This action return a #${id} cat`;
     }
 
     /*    @Get('ab*cd')
