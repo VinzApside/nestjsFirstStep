75d806e60abbfbdd607865a41555624cee0879b6
Vinz <vincentballut@outlook.fr>
2019-07-04 10:18:49 +0200

    add post
    with a header and specific response code

--- project-name/src/cats/cats.controller.ts
+++ project-name/src/cats/cats.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, Header } from '@nestjs/common';
 import { get } from 'http';
+import { create } from 'domain';
 
 @Controller('cats')
 export class CatsController {
@@ -8,4 +9,16 @@ export class CatsController {
         return 'This action returns all cats';
     }
 
+    /*    @Get('ab*cd')
+        findAll() {
+          return 'This route uses a wildcard';
+        }
+    */
+
+    @Post()
+    @HttpCode(204)
+    @Header('Cache-Control', 'none')
+    create(): string {
+        return 'This action adds a new cat';
+    }
 }
